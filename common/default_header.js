d676d4453e1e818dee76caf8e54d16250f3f0b00
jd <jd@cashmallow.com>
2025-06-25 02:42:14 +0900

    fix(header): remove default header component from main layout

--- common/default_header.js
+++ common/default_header.js
@@ -10,8 +10,8 @@ class DefaultHeader extends HTMLElement {
           <span class="text-xl font-bold text-gray-900 select-none">${title}</span>
         </div>
         <div class="flex items-center gap-2 min-w-[56px] justify-end z-10">
-          ${showHome ? '<button onclick="location.href=\'main.html\'" class="w-7 h-7 flex items-center justify-center"><img src="../images/home_black.png" alt="홈" class="w-[20px] h-[20px] object-contain" /></button>' : ''}
-          ${showCart ? '<button onclick="location.href=\'cart.html\'" class="w-7 h-7 flex items-center justify-center"><img src="../images/cart_black.png" alt="장바구니" class="ml-3 w-[23px] h-[21px] object-contain" /></button>' : ''}
+          ${showHome ? '<button onclick="location.href=\'main.html\'" class="w-7 h-7 flex items-center justify-center"><img src="images/home_black.png" alt="홈" class="w-[20px] h-[20px] object-contain" /></button>' : ''}
+          ${showCart ? '<button onclick="location.href=\'cart.html\'" class="w-7 h-7 flex items-center justify-center"><img src="images/cart_black.png" alt="장바구니" class="ml-3 w-[23px] h-[21px] object-contain" /></button>' : ''}
         </div>
       </div>
     `;
