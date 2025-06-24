b7d9dd05d902904b7df0758d30eda2aef83be4c1
jd <jd@cashmallow.com>
2025-06-25 03:46:44 +0900

    fix(header): remove default header component from main layout

--- common/default_header.js
+++ common/default_header.js
@@ -7,7 +7,7 @@ class DefaultHeader extends HTMLElement {
       <div class="w-full flex items-center h-14 px-4 bg-white relative">
         <button onclick="history.back()" class="text-2xl text-gray-800 mr-auto z-10">&#8592;</button>
         <div class="absolute left-0 right-0 flex justify-center items-center h-14 pointer-events-none">
-          <span class="text-xl font-bold text-gray-900 select-none">${title}</span>
+          <span class="text font-bold text-gray-900 select-none">${title}</span>
         </div>
         <div class="flex items-center gap-2 min-w-[56px] justify-end z-10">
           ${showHome ? '<button onclick="location.href=\'main.html\'" class="w-7 h-7 flex items-center justify-center"><img src="images/home_black.png" alt="홈" class="w-[20px] h-[20px] object-contain" /></button>' : ''}
